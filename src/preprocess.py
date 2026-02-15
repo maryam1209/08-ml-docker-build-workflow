a74af222002bfc27c7f0d6a8de61f921caa5c5c4
maryam1209 <168336954+maryam1209@users.noreply.github.com>
2026-02-15 23:17:23 +0500

    Add docstring to load_and_clean function
    Added docstring to load_and_clean function.

--- src/preprocess.py
+++ src/preprocess.py
@@ -1,6 +1,8 @@
-import pandas as pd
 
 def load_and_clean(file_path):
+    """
+    Load CSV and drop rows with missing values.
+    """
     df = pd.read_csv(file_path)
     df_clean = df.dropna()
     return df_clean
